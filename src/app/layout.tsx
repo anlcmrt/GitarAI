de23e89a932eb40cd69ee9061fc2553efd04cb35
Anil Mert Cömert <anil.coemert@gmail.com>
2025-07-11 14:27:51 +0200

    layout düzenlendi

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
+"use client";
+
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
-import Link from "next/link"; // Footer'daki Link için gerekli
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,12 +27,16 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const pathname = usePathname();
+
+  const isSongsPage = pathname?.startsWith("/songs");
+
   return (
     <html lang="tr">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased
-          bg-neutral-900 text-white min-h-screen flex flex-col
-          sm:bg-gray-50 sm:text-gray-900`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col ${
+          isSongsPage ? "bg-neutral-900 text-white" : "bg-gray-50 text-gray-900"
+        }`}
       >
         <Navbar />
         <main className="flex-grow">{children}</main>
